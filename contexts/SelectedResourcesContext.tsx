4cc3e76ba0e8d4a3453a406f44dffb9580675742
ali.n <anorouzi.work@gmail.com>
2025-02-22 02:28:55 +0330

    Feat: add resource toast.

--- contexts/SelectedResourcesContext.tsx
+++ contexts/SelectedResourcesContext.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { ToastAction } from '@/components/ui/toast';
 import { useToast } from '@/hooks/use-toast';
 import type { TSelectedResource } from '@/types/resources';
 import type React from 'react';
 import { createContext, useContext, useState } from 'react';
+import Image from "next/image";
 
 type SelectedResourcesContextType = {
   selectedResources: TSelectedResource[];
@@ -32,12 +34,36 @@ export function SelectedResourcesProvider({
       if (prev.length >= 10) {
         toast({
           variant: 'destructive',
-          title: 'Limit reached',
           description: 'You can only select up to 10 resources.'
         });
         return prev; // Prevent adding more than 10 resources
       }
       if (!prev.some((r) => r.id === resource.id)) {
+        toast({
+          variant: 'default',
+          description:  (
+              <div className="flex align-center gap-2">
+                <Image
+                    width={24}
+                    height={24}
+                    src={ resource?.image_url || '/placeholder.svg' }
+                    alt="Toast Image"
+                    style={{ marginRight: 8, borderRadius: '50%' }}
+                />
+                Added to the resources.
+              </div>
+          ),
+          action: <ToastAction className="bg-primary text-black border-none font-semibold text-xs rounded-[6px] hover:bg-primary/90" altText="Remove" onClick={() => removeResource(resource.id)}>Remove</ToastAction>,
+          style: {
+            position: 'fixed',
+            bottom: '20px',
+            left: '20px',
+            maxWidth: 'max-content',
+            paddingRight: '34px',
+            paddingLeft: '16px',
+          },
+          className: 'p-4 bg-foreground border-none text-black text-[13px] rounded-lg',
+        });
         return [...prev, resource];
       }
       return prev;
